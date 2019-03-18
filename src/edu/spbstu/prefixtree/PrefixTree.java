a5d2cb655c85b8e76b8a81cbc37d5a365792ebee
Анна Александра <annakoorkoff@gmail.com>
2019-03-18 03:29:06 +0300

    fixes&stuff

--- src/edu/spbstu/prefixtree/PrefixTree.java
+++ src/edu/spbstu/prefixtree/PrefixTree.java
@@ -121,28 +121,4 @@ public class PrefixTree {
         }
     }
 
-
-    public static void main(String[] args) {
-        System.out.println("Prefix tree");
-
-        PrefixTree prefixTree = new PrefixTree();
-        prefixTree.add("i");
-        prefixTree.add("is");
-        prefixTree.add("issue");
-        prefixTree.add("can");
-        prefixTree.add("cat");
-        prefixTree.add("canada");
-        prefixTree.add("cant");
-        prefixTree.add("volume");
-        prefixTree.add("value");
-
-        List<String> found = prefixTree.findAllWithPrefix("can");
-        for (String s : found) {
-            System.out.println(s);
-        }
-
-        System.out.println("prefixTree.findStr = " + prefixTree.findStr("cant"));
-        prefixTree.remove("cant");
-        System.out.println("prefixTree.findStr = " + prefixTree.findStr("cant"));
-    }
 }
--- test/edu/spbstu/prefixtree/PrefixTreeTest.java
+++ test/edu/spbstu/prefixtree/PrefixTreeTest.java
@@ -24,18 +24,55 @@ public class PrefixTreeTest {
         prefixTree.add("cant");
         prefixTree.add("volume");
         prefixTree.add("value");
-        assertEquals(10, prefixTree.findAllWithPrefix("").size());
+        assertEquals(9, prefixTree.findAllWithPrefix("").size());
     }
 
     @Test
     public void findAllWithPrefix() {
+        prefixTree.add("i");
+        prefixTree.add("is");
+        prefixTree.add("issue");
+        prefixTree.add("can");
+        prefixTree.add("cat");
+        prefixTree.add("canada");
+        prefixTree.add("voice");
+        prefixTree.add("volume");
+        prefixTree.add("value");
+        assertEquals(3, prefixTree.findAllWithPrefix("i").size());
+        assertEquals(2, prefixTree.findAllWithPrefix("is").size());
+        assertEquals(2, prefixTree.findAllWithPrefix("can").size());
+        assertEquals(3, prefixTree.findAllWithPrefix("ca").size());
+        assertEquals(3, prefixTree.findAllWithPrefix("v").size());
+        assertEquals(1, prefixTree.findAllWithPrefix("val").size());
+        assertEquals(2, prefixTree.findAllWithPrefix("vo").size());
+        assertEquals(3, prefixTree.findAllWithPrefix("c").size());
     }
 
     @Test
     public void findStr() {
+        prefixTree.add("highest");
+        prefixTree.add("high");
+        prefixTree.add("memes");
+        prefixTree.add("memory");
+        prefixTree.add("corn");
+        assertEquals(false, prefixTree.findStr("memo"));
+        assertEquals(false, prefixTree.findStr("core"));
+        assertEquals(true, prefixTree.findStr("high"));
+        assertEquals(false, prefixTree.findStr("highestr"));
     }
 
     @Test
     public void remove() {
+        prefixTree.add("highest");
+        prefixTree.add("high");
+        prefixTree.remove("highest");
+        assertTrue(prefixTree.findStr("high"));
+        assertFalse(prefixTree.findStr("heinz"));
+        assertFalse(prefixTree.findStr("store"));
+        assertFalse(prefixTree.findStr("highest"));
+        prefixTree.add("warning");
+        prefixTree.remove("warning");
+        assertFalse(prefixTree.findStr("warning"));
+        assertTrue(prefixTree.findStr("high"));
     }
 }
